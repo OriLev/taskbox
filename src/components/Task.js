3f2f17f5dc2e9bc1e8a315ce7ef6559bf9f1773d
Ori Levitan <ori.levitan@herolo.co.il>
2018-11-08 13:50:45 +0200

    taskbox UI - static deployement

--- src/components/Task.js
+++ src/components/Task.js
@@ -17,7 +17,7 @@ export default function Task({
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
-      <div className="title">
+      <div className="title" style={{ background: "papayawhip"}}>
         <input
           type="text"
           value={title}
